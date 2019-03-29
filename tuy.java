785f0259622758501f9aa296e85a4b308cdfc133
chantal123 <49054682+chantal123@users.noreply.github.com>
2019-03-29 08:11:11 +0200

    Add files via upload

---
+++ tuy.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+
+class employee	
+{
+static String name ;
+static int phone;
+static String address;
+static int salary;
+static int hours;
+static void getdata()
+{
+Scanner k= new Scanner (System.in);
+int op;
+System.out.println("select option:[1]monthly,[2]weekly,[3]hourly");
+op=k.nextInt();
+if(op==1)
+{
+System.out.println("enter name");
+name=k.next();
+System.out.println("enter phone");
+phone=	k.nextInt();
+System.out.println("enter address");
+address=k.next();
+System.out.println("enter salary");
+salary=k.nextInt();
+int result;
+result=salary-(((salary*2)/100)+((salary*5)/100));
+System.out.println("your salary ="+result);
+}
+else if(op==2)
+{
+System.out.println("enter name");
+name=k.next();
+System.out.println("enter phone");
+phone=	k.nextInt();
+System.out.println("enter address");
+address=k.next();
+System.out.println("enter hourly rate");
+salary=k.nextInt();
+System.out.println("enter hours");
+hours=k.nextInt();
+
+int result;
+result=(salary*hours)-((((salary*hours)*30)/100)+(((salary*hours)*3)/100));
+System.out.println("your salary ="+result);		
+}	
+else if(op==3)
+{
+System.out.println("enter name");
+name=k.next();
+System.out.println("enter phone");
+phone=	k.nextInt();
+System.out.println("enter address");
+address=k.next();
+System.out.println("enter salary");
+salary=k.nextInt();
+int result;
+result=salary;s
+System.out.println("your salary ="+result);	
+}
+else
+{
+System.out.println("you enering imvalid option");
+}
+}
+
+		
+public static void main(String args[])
+{
+getdata();
+}
+}
